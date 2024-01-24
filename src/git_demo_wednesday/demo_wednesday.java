850b66f6ca69bd3036473d179a8659938908a095
Ritika6283 <sainiritika6283@gmail.com>
2024-01-24 09:00:59 -0500

    New class

---
+++ src/git_demo_wednesday/demo_wednesday.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git_demo_wednesday;
+
+/**
+ *
+ * @author lenovo
+ */
+public class demo_wednesday {
+    
+}
